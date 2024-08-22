5ec9ba94a576fe45cb461095c86f9d5819058fbd
Mn+7 <113846020+AC-Mnky@users.noreply.github.com>
2024-08-22 15:22:49 +0800

    vision2 can now calculate the cords of the wall

--- src/vision2/main.py
+++ src/vision2/main.py
@@ -7,13 +7,16 @@ import camera_convert
 
 MODE = 'file'
 # MODE = 'camera'
-SHOW = True
-READ_DIR = 'wall2'
+GLOBAL_SHOW = True
+MASK_SHOW = False
+READ_DIR = 'wall4'
 WRITE_DIR = 'wall5'
+
+CAMERA_STATE = camera_convert.CameraState((230, 0, -180), (90 - 27, 0), (62.2, 48.8), (640, 480))
 
 SHOW_RED = SHOW_YELLOW = True
 SHOW_WALL = True
-DRAW_GRID = False
+DRAW_GRID = True
 
 USE_HOUGH_P = True
 
@@ -22,19 +25,19 @@ if MODE == 'camera':
 
 
 def process(img, show: bool = False):
-    points_red = find_color.find_red(img, show)
-    points_yellow = find_color.find_yellow(img, show)
-    walls = find_color.find_wall_bottom_p(img, show) if USE_HOUGH_P else \
-        find_color.find_wall_bottom(img, show)
+    points_red = find_color.find_red(img, show and MASK_SHOW)
+    points_yellow = find_color.find_yellow(img, show and MASK_SHOW)
+    walls = find_color.find_wall_bottom_p(img, show and MASK_SHOW) if USE_HOUGH_P else \
+        find_color.find_wall_bottom(img, show and MASK_SHOW)
 
     if DRAW_GRID:
-        draw_grid((255, 255, 255, 255), 0, 1500, 50, -1000, 1000, 50)
+        draw_grid((255, 255, 255, 255), 0, 2000, 50, -1000, 1000, 50)
 
     print()
 
     if SHOW_RED:
         for p in points_red:
-            s, x, y = camera_convert.img2space(camera_state, p[0], p[1], -12.5)
+            s, x, y = camera_convert.img2space(CAMERA_STATE, p[0], p[1], -12.5)
             if s:
                 cv2.rectangle(img, (p[0] - 10, p[1] - 10, 20, 20), (255, 255, 255, 255), 2)
                 print((x, y), 'red')
@@ -43,7 +46,7 @@ def process(img, show: bool = False):
 
     if SHOW_YELLOW:
         for p in points_yellow:
-            s, x, y = camera_convert.img2space(camera_state, p[0], p[1], -15)
+            s, x, y = camera_convert.img2space(CAMERA_STATE, p[0], p[1], -15)
             if s:
                 cv2.circle(img, p, 10, (255, 255, 255, 255), 2)
                 print((x, y), 'yellow')
@@ -51,13 +54,10 @@ def process(img, show: bool = False):
                 cv2.circle(img, p, 10, (128, 128, 128, 255), 1)
 
     if SHOW_WALL and walls is not None:
-        if USE_HOUGH_P:
-            for w in walls:
+        for w in walls:
+            if USE_HOUGH_P:
                 h1, v1, h2, v2 = w[0]
-                cv2.line(img, (h1, v1), (h2, v2), (255, 255, 255, 255), 1)
-                print(((h1, v1), (h2, v2)), 'wall')
-        else:
-            for w in walls:
+            else:
                 rho, theta = w[0]
                 a = np.cos(theta)
                 b = np.sin(theta)
@@ -67,33 +67,45 @@ def process(img, show: bool = False):
                 v1 = int(v0 + 1000 * a)
                 h2 = int(h0 - 1000 * (-b))
                 v2 = int(v0 - 1000 * a)
-                cv2.line(img, (h1, v1), (h2, v2), (255, 255, 255, 255), 1)
-                s1, x1, y1 = camera_convert.img2space(camera_state, h1, v1, 0)
-                s2, x2, y2 = camera_convert.img2space(camera_state, h2, v2, 0)
-                print(((x1, y1), (x2, y2)), 'wall')
+            cv2.line(img, (h1, v1), (h2, v2), (255, 255, 255, 255), 1)
+            s1, x1, y1 = camera_convert.img2space(CAMERA_STATE, h1, v1, 0)
+            s2, x2, y2 = camera_convert.img2space(CAMERA_STATE, h2, v2, 0)
+            print(((x1, y1), (x2, y2)), 'wall')
 
     if show:
         cv2.imshow('image', img)
 
 
 def draw_grid(color, x_start, x_stop, x_step, y_start, y_stop, y_step):
+    overlay = np.zeros(image.shape, np.uint8)
+
     for x in range(x_start, x_stop + x_step, x_step):
         for y in range(y_start, y_stop, y_step):
-            s1, i1, j1 = camera_convert.space2img(camera_state, x, y)
-            s2, i2, j2 = camera_convert.space2img(camera_state, x, y + y_step)
+            s1, i1, j1 = camera_convert.space2img(CAMERA_STATE, x, y)
+            s2, i2, j2 = camera_convert.space2img(CAMERA_STATE, x, y + y_step)
             if s1 or s2:
-                cv2.line(image, (i1, j1), (i2, j2), color, 1)
+                cv2.line(overlay, (i1, j1), (i2, j2), color, 1)
     for y in range(y_start, y_stop + y_step, y_step):
         for x in range(x_start, x_stop, x_step):
-            s1, i1, j1 = camera_convert.space2img(camera_state, x, y)
-            s2, i2, j2 = camera_convert.space2img(camera_state, x + x_step, y)
+            s1, i1, j1 = camera_convert.space2img(CAMERA_STATE, x, y)
+            s2, i2, j2 = camera_convert.space2img(CAMERA_STATE, x + x_step, y)
             if s1 or s2:
-                cv2.line(image, (i1, j1), (i2, j2), color, 1)
+                cv2.line(overlay, (i1, j1), (i2, j2), color, 1)
+
+    overlay = np.minimum(overlay,
+                         np.repeat((255 - find_color.get_color_mask(image, find_color.RED))[:, :, np.newaxis], 3,
+                                   axis=2))
+    overlay = np.minimum(overlay,
+                         np.repeat((255 - find_color.get_color_mask(image, find_color.YELLOW))[:, :, np.newaxis], 3,
+                                   axis=2))
+    overlay = np.minimum(overlay,
+                         np.repeat((255 - find_color.get_color_mask(image, find_color.BLUE))[:, :, np.newaxis], 3,
+                                   axis=2))
+
+    cv2.add(overlay, image, image)
 
 
 if __name__ == "__main__":
-
-    camera_state = camera_convert.CameraState((100, 0, -90), (70, 0), (64, 50), (640, 480))
 
     repository_path = os.path.dirname(os.path.realpath(__file__)) + '/../..'
     if MODE == 'file':
@@ -115,5 +127,5 @@ if __name__ == "__main__":
             filename = repository_path + '/assets/openCV_pic/' + WRITE_DIR + '/' + str(
                 image_index) + '.jpg'
             cv2.imwrite(filename, image)
-            process(image, SHOW)
+            process(image, GLOBAL_SHOW)
             cv2.waitKey(500)
