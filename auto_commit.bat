75db85b63db75740bd5690b2be010edb2460d794
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-08-23 15:58:24 +0800

    Change bat files.

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,4 +1,10 @@
 @echo off
+for /f "tokens=1-4 delims=:. " %%a in ("%time%") do (
+    set hour=%%a
+    set min=%%b
+    set sec=%%c
+)
+echo Current Time is %hour%:%min%:%sec%. Hello from Trium!
 echo Auto pull changes from remote repository.
 git pull
 echo Pull success, start to commit changes.
