1566177c7eef0e154abfcc981d0fed19b788094f
Mn+7 <113846020+AC-Mnky@users.noreply.github.com>
2024-08-23 11:01:54 +0800

    added dummy

--- src/main_control.py
+++ src/main_control.py
@@ -1,35 +1,34 @@
 import time
-from communication import stm_communication as stm
-from communication import imu
 
-from algorithm import vision
-from algorithm import core
-
-import dummy
-
+ENABLE_COMMUNICATION = True  # True
 ENABLE_CAMERA = False  # True
 ENABLE_CORE = True  # True
-
-USE_DUMMY = False  # False
+USE_DUMMY = True  # False
 
 CAMERA_COOLDOWN = 0.5
 
+if ENABLE_COMMUNICATION:
+    from communication import stm_communication as stm
+    from communication import imu
 if ENABLE_CAMERA:
     from communication import camera
+    from algorithm import vision
+if ENABLE_CORE:
+    from algorithm import core
+if USE_DUMMY:
+    import dummy
 
 if __name__ == '__main__':
 
     start_time = time.time()
 
-    stm = stm.STM()
-    imu = imu.IMU()
-
-    if ENABLE_CAMERA:
-        camera = camera.Camera()
-    else:
-        camera = None
-
     output = None
+    encoder_and_ultrasonic_input = None
+    imu_input = None
+
+    stm = stm.STM() if ENABLE_COMMUNICATION else None
+    imu = imu.IMU() if ENABLE_COMMUNICATION else None
+    camera = camera.Camera() if ENABLE_CAMERA else None
     core = core.Core() if ENABLE_CORE else None
     dummy = dummy.Dummy() if USE_DUMMY else None
 
@@ -42,10 +41,12 @@ if __name__ == '__main__':
         time.sleep(0.001)
         print(time.time() - start_time)
 
-        encoder_and_ultrasonic_input = stm.get_encoder_and_ultrasonic_input()
-        imu_input = imu.get_imu_input()
-
         camera_input = None
+
+        if ENABLE_COMMUNICATION:
+            encoder_and_ultrasonic_input = stm.get_encoder_and_ultrasonic_input()
+            imu_input = imu.get_imu_input()
+
         if ENABLE_CAMERA and time.time() - camera_last_used_time > CAMERA_COOLDOWN:
             camera_image = camera.get_image_bgr()
             camera_last_used_time = time.time()
@@ -58,4 +59,3 @@ if __name__ == '__main__':
             output = dummy.get_output()
 
         stm.send_output(output)
-
