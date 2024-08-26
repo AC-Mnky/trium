560fb16af4f0bbe52f1a676e53e713aab13bb689
Mn+7 <acmnky2022@outlook.com>
2024-08-26 17:53:09 +0800

    Minor changes.

--- src/dummy.py
+++ src/dummy.py
@@ -59,7 +59,7 @@ class Dummy:
         self.right_mouse_offset = None
         self.mouse_on_text = None
 
-        self.motor_PID = [[15, 10, 40, 10, 0, 10, 5, 0], [15, 10, 40, 10, 0, 10, 5, 0]]
+        self.motor_PID = [[15, 10, 40, 5, 0, 10, 5, 0], [15, 10, 40, 5, 0, 10, 5, 0]]
 
         self.stm_input = bytes((0,) * 96)
 
