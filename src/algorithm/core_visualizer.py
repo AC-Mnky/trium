d278121e797e8c80f1738f7dc9a6df22ef517d59
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-08-28 22:54:45 +0800

    Better docs.

--- src/algorithm/core_visualizer.py
+++ src/algorithm/core_visualizer.py
@@ -122,7 +122,10 @@ class Visualizer:
     def draw(self) -> None:
         """
         Draws the visualization of the algorithm on the screen.
-        This method fills the screen with a background color and then draws various shapes and lines to represent the algorithm's predicted items, walls, and other elements. The visualization includes circles, lines, and polygons.
+
+        - This method fills the screen with a background color and then draws various shapes and
+        lines to represent the algorithm's predicted items, walls, and other elements.
+        - The visualization includes circles, lines, and polygons.
 
         Returns:
             None
