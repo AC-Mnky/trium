faafdf1d345cb99907f798ed6e193bb6337fade7
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-08-28 22:31:33 +0800

    Better docs.

--- src/algorithm/camera_ori.py
+++ src/algorithm/camera_ori.py
@@ -14,6 +14,12 @@ class Camera:
         self.start_time = time.time()
 
     def get_image_bgr(self) -> cv2.UMat | None:
+        """
+        Retrieves the BGR image from the camera.
+
+        Returns:
+            cv2.UMat | None: The BGR image if available, None otherwise.
+        """
         if time.time() - self.start_time < START_WAIT_TIME:
             return None
         return cv2.cvtColor(self.cam.capture_array("main"), cv2.COLOR_RGB2BGR)
