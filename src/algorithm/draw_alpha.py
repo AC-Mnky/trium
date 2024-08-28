ea6252e55d7796f3d43e4b7229c939d343d2283a
Mn+7 <acmnky2022@outlook.com>
2024-08-28 15:43:46 +0800

    Add docs
    Co-authored-by: VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
    Co-authored-by: chaoyiL <42493073+chaoyiL@users.noreply.github.com>

--- src/algorithm/draw_alpha.py
+++ src/algorithm/draw_alpha.py
@@ -3,7 +3,7 @@ import pygame
 
 def rectangle(
     surface: pygame.Surface,
-    color: tuple[Literal[255], Literal[255], Literal[255], Literal[255]],
+    color: tuple,
     rect: tuple,
 ) -> None:
     """
@@ -30,11 +30,11 @@ def line(
     width: float,
 ) -> None:
     """
-    Draw a line on the given surface.
+    Draw a line on the given surface with the specified color and alpha.
 
     Args:
         surface (pygame.Surface): The surface to draw the line on.
-        color (tuple): The color of the line in RGB format.
+        color (tuple): The color of the line.
         start_pos (tuple): The starting position of the line.
         end_pos (tuple): The ending position of the line.
         width (float): The width of the line.
@@ -59,14 +59,37 @@ def line(
     surface.blit(shape_surf, target_rect)
 
 
-def circle(surface, color, center, radius):
+def circle(surface: pygame.Surface, color:tuple, center:tuple, radius: float) -> None:
+    """
+    Draw a circle on the given surface with the specified color and alpha.
+
+    Args:
+        surface (pygame.Surface): The surface to draw the circle on.
+        color (tuple): The color of the circle.
+        center (tuple): The center coordinates of the circle.
+        radius (float): The radius of the circle.
+
+    Returns:
+        None
+    """
     target_rect = pygame.Rect(center, (0, 0)).inflate((radius * 2, radius * 2))
     shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
     pygame.draw.circle(shape_surf, color, (radius, radius), radius)
     surface.blit(shape_surf, target_rect)
 
 
-def polygon(surface, color, points):
+def polygon(surface: pygame.Surface, color: tuple, points: list) -> None:
+    """
+    Draw a polygon on the given surface with the specified color and alpha.
+
+    Args:
+        surface (pygame.Surface): The surface to draw the polygon on.
+        color (tuple): The color of the polygon.
+        points (list): The list of points that define the polygon.
+
+    Returns:
+        None
+    """
     lx, ly = zip(*points)
     min_x, min_y, max_x, max_y = min(lx), min(ly), max(lx), max(ly)
     target_rect = pygame.Rect(min_x, min_y, max_x - min_x, max_y - min_y)
