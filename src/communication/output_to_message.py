8215f22b12c0f28841e1095655dcf3bc256a232d
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-08-30 00:28:07 +0800

    Better docs.

--- src/communication/output_to_message.py
+++ src/communication/output_to_message.py
@@ -2,6 +2,15 @@ PWM_PERIOD = 100
 
 
 def output_to_message(output) -> bytes:
+    """
+    Converts the given output to a message in bytes format.
+
+    Args:
+        output (list): The output to be converted.
+
+    Returns:
+        bytes: The converted message in bytes format.
+    """
     message = (
         [
             0x80,
