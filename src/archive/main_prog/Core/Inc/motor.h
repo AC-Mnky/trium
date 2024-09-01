26639e858c1511b253e31fb8008c64e46f8359fa
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-09-01 13:22:26 +0800

    Update motor control.

--- src/archive/main_prog/Core/Inc/motor.h
+++ src/archive/main_prog/Core/Inc/motor.h
@@ -4,6 +4,7 @@
 void motor_init(void);
 void set_motor_speed(uint8_t, int);
 void set_servo_angle(uint16_t);
+void set_servo_angle_delay(uint16_t pulse);
 
 uint16_t get_encoder_CNT(int, uint32_t*);
 uint8_t get_encoder_direction(uint8_t);
