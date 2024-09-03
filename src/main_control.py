32413b97fff2e188911d8720ee418b565f8c2a89
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-09-04 01:33:43 +0800

    chore: auto format code after commit

--- src/main_control.py
+++ src/main_control.py
@@ -6,7 +6,7 @@ parser.add_argument("-d", dest="disable", action="store_true", help="Disable cor
 parser.set_defaults(disable=False)
 args = parser.parse_args()
 
-ENABLE_STM_INPUT =  True  # True
+ENABLE_STM_INPUT = True  # True
 STM_INPUT_PROTOCOL = 127  # 127 | 128
 ENABLE_IMU = True  # True
 ENABLE_CAMERA = True  # True
